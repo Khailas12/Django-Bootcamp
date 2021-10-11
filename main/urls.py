4ea9aefdd34508640398294dab16053e84e5d539
Khailas R <khailas303@gmail.com>
2021-10-11 17:59:18 +0530

    minor fix in the pk

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from myapp.views import home_view, product_detail_view, product_json_view
 
 
 urlpatterns = [
-    path('search/', home_view, name='home'),
+    path('', home_view, name='home'),
     path('product/<int:pk>/', product_detail_view, name='productdetails'),  # pk -> primary key
     # path('productjson/<int:id>/', product_json_view, name='productjson'),
     re_path(r'productjson/(?P<pk>\d+)/', product_json_view, name='prodjson'),
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from myapp.models import Product
 
 
 def home_view(request, *args, **kwargs):
+    print(args, kwargs)
     return HttpResponse("<h1>Hello world</h1>")
 
 
@@ -15,12 +16,12 @@ def product_detail_view(request, pk, *args, **kwargs):
         obj = Product.objects.get(pk=pk)
     except Product.DoesNotExist:    # this handles whether it exists or not
         raise Http404('Product does not exist')
-    return HttpResponse(f'Product pk {obj.pk}')
+    return HttpResponse(f'Product ID {obj.pk}')
 
 
 def product_json_view(request, pk,  *args, **kwargs):    
     try:
         obj = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
-        return JsonResponse({'message': '404 Not Found'})
-    return JsonResponse({'pk': obj.pkproduct})
+        return JsonResponse({'message': 'Not Found'})
+    return JsonResponse({'pk': obj.pk})

a9aaa07c8adb774816fc143eb36c1ce56160820a
Khailas R <khailas303@gmail.com>
2021-10-18 01:42:42 +0530

    Revert "updated"
    This reverts commit fdf1e2ef42eced4a1c7c164d66cf101dbf172aa6.

--- myapp/models.py
+++ myapp/models.py
@@ -10,6 +10,11 @@ class Product(models.Model):
     
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE) # it deletes everything including the existing object once the user is deleted
     
+<<<<<<< HEAD
+=======
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    
+>>>>>>> parent of fdf1e2e (updated)
     title = models.CharField(max_length=120)
     content = models.TextField(null=True, blank=False)
     price = models.DecimalField(

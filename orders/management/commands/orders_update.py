26ae20834e48b1d4eb5bebd3a798b71635c8db34
Khailas R <khailas303@gmail.com>
2021-10-17 15:56:44 +0530

    basic update

--- orders/management/commands/orders_update.py
+++ orders/management/commands/orders_update.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-import datetime
 from orders.models import Order
+import datetime
 
 
 class Command(BaseCommand):
@@ -9,6 +9,7 @@ class Command(BaseCommand):
         now = timezone.now()
         today_start = now.replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=2)
         today_end = now.replace(hour=23, minute=59, second=59, microsecond=99999) - datetime.timedelta(days=2)
+
+        # timestamp__gte = greater than or equal to and lte = less than or equal to
         queryset = Order.objects.filter(timestamp__gte=today_start, timestamp__lte=today_end, status='created')
         queryset.update(status='stale')
-        
--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from myapp.models import Product
 from django.db.models.signals import pre_save, post_save
 from decimal import Decimal
 
+
 User = get_user_model()
 
 ORDER_STATUS_CHOICES = (
@@ -36,7 +37,7 @@ class Order(models.Model):
         paid_amount = self.total
         if custom_amount != None:
             paid_amount = custom_amount
-            
+
         self.paid = paid_amount
         self.status = 'paid'
         

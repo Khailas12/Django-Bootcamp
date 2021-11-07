fa6971e6454623644923bb04da4b4d972fbab47b
Khailas R <khailas303@gmail.com>
2021-11-07 19:07:36 +0530

    register updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,13 +2,22 @@ from django.contrib.auth import get_user_model
 from django import forms
 
 
-User = get_user_model()     #fetches info from the db to validate whether the user's existance
+# fetches info from the db to validate whether the user's existance
+User = get_user_model()
 
 not_allowed_username = ['abc']
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=60)
+    username = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',                'id': 'username'
+            }
+        )
+    )
+    
     email = forms.EmailField(required=True)
     password1 = forms.CharField(
         label='Password',
@@ -19,7 +28,7 @@ class RegisterForm(forms.Form):
             }
         )
     )
-    
+
     password2 = forms.CharField(
         label='Confirm Password',
         widget=forms.PasswordInput(
@@ -29,29 +38,29 @@ class RegisterForm(forms.Form):
             }
         )
     )
-    
+
     def __str__(self):
         return self.username
-    
+
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        queryset = User.objects.filter(username__iexact=username) 
-        
+        queryset = User.objects.filter(username__iexact=username)
+
         if username in not_allowed_username:
             raise forms.ValidationError('Invalid Username, try another one')
-        
+
         if queryset.exists():
-            raise forms.ValidationError('User already exists, Please Login instead')
+            raise forms.ValidationError(
+                'User already exists, Please Login instead')
         return username
-    
-    
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         queryset = User.objects.filter(email__iexact=email)
-        
+
         if queryset.exists():
             return forms.ValidationError('This email already exists, try another one')
-            
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -63,23 +72,21 @@ class LoginForm(forms.Form):
             }   # html attributes
         )
     )
-    
+
     # to authenticate and verifiy
-    # def clean(self):    
-    #     data = super().clean() 
+    # def clean(self):
+    #     data = super().clean()
     #     username = data.get('username')
     #     password = data.get('password')
 
-
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        queryset = User.objects.filter(username__iexact=username) # iextract -> username == username
-        
+        # iextract -> username == username
+        queryset = User.objects.filter(username__iexact=username)
+
         if not queryset.exists():
             raise forms.ValidationError('This is an Invalid User')
 
         if queryset.count != 1:
             raise forms.ValidationError('This is an invalid user')
         return username
-    
-# for usename and email
